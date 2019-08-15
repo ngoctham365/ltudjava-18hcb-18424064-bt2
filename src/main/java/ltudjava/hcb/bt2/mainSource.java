455c34dde8570600047206a88fe45da80569fd4f
ngoctham365 <ngoctham365@gmail.com>
2019-08-15 22:33:31 +0700

    áp dụng hibernate

--- src/main/java/ltudjava/hcb/bt2/mainSource.java
+++ src/main/java/ltudjava/hcb/bt2/mainSource.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ltudjava.hcb.bt2;
 
-/**
- *
- * @author 
- */
 public class mainSource {
     public static void main(String[] args) {
         System.out.println("abdfguwe");
