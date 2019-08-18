a4ad8db1804bf36ece2f68c6e4ddfa0b918c762d
ngoctham365 <ngoctham365@gmail.com>
2019-08-18 19:47:42 +0700

    cập nhât hàm hỗ trợ

--- src/main/java/ltudjava/hcb/bt2/bus/HelperBUS.java
+++ src/main/java/ltudjava/hcb/bt2/bus/HelperBUS.java
@@ -5,11 +5,24 @@
  */
 package ltudjava.hcb.bt2.bus;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Formatter;
+import java.util.List;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
@@ -71,8 +84,40 @@ public class HelperBUS {
     public static String concatWithIconMoveRight(String input1, String input2) {
         return input1 + iconMoveRightHasSpace() + input2;
     }
-    
+
     public static String[] concatWithIconMoveRight(String input) {
         return input.split(iconMoveRightHasSpace());
+    }
+
+    public static List<String> openFileToGetContent(JFrame jFrame) {
+        List<String> strings = new ArrayList<String>();
+        JFileChooser fileChooser = new JFileChooser("./data");
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("CSV File", "csv"));
+        fileChooser.setAcceptAllFileFilterUsed(true);
+        if (fileChooser.showOpenDialog(jFrame) == JFileChooser.APPROVE_OPTION) {
+            strings.add(fileChooser.getSelectedFile().getName().replace(".csv", ""));
+
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileChooser.getSelectedFile()), "UTF8"));
+                String contentFile;
+                while ((contentFile = reader.readLine()) != null) {
+                    strings.add(contentFile);
+                }
+
+                reader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return strings;
+    }
+
+    public static Float convertFloat(String input) {
+        try {
+            return Float.parseFloat(input);
+        } catch (Exception e) {
+        }
+        return null;
     }
 }
