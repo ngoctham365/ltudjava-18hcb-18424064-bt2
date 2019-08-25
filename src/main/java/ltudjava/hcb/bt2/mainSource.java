33c9e3ef4974c6b28d02a9a618e86aee26868847
ngoctham365 <ngoctham365@gmail.com>
2019-08-25 20:38:38 +0700

    export file initialize by file mainSource.java

--- src/main/java/ltudjava/hcb/bt2/mainSource.java
+++ src/main/java/ltudjava/hcb/bt2/mainSource.java
@@ -1,14 +1,35 @@
 package ltudjava.hcb.bt2;
 
-import ltudjava.hcb.bt2.bus.*;
-import ltudjava.hcb.bt2.dao.*;
-import ltudjava.hcb.bt2.dto.*;
+import ltudjava.hcb.bt2.gui.LogInFrame;
 
 public class mainSource {
-    public static void main(String[] args) {
-        HelperBUS.openFileToGetContent(null);
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(LogInFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
         
-        System.out.println("end!!!");
-        HibernateUtil.getSessionFactory().close();
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                //new LoginJFrame().setVisible(true);
+                LogInFrame f = new LogInFrame();
+                f.setVisible(true);
+                f.setLocationRelativeTo(null);
+                f.setResizable(false);
+            }
+        });
     }
 }
